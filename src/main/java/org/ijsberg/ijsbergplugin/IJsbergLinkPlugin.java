a37bbe84cfc622906f56d426b2af5a33e5fcfb20
jmeetsma <jeroen@ijsberg.nl>
2014-12-05 04:30:30 +0100

    created upload tasks

--- src/main/java/org/ijsberg/ijsbergplugin/IJsbergLinkPlugin.java
+++ src/main/java/org/ijsberg/ijsbergplugin/IJsbergLinkPlugin.java
@@ -1,6 +1,7 @@
 package org.ijsberg.ijsbergplugin;
 
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -9,12 +10,17 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.FormValidation;
 import net.sf.json.JSONObject;
+import org.ijsberg.iglu.util.io.FileSupport;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import javax.servlet.ServletException;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Sample {@link Builder}.
@@ -24,7 +30,7 @@ import java.io.IOException;
  * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
  * and a new {@link IJsbergLinkPlugin} is created. The created
  * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
+ * XStream, so this allows you to use instance fields (like {@link #customerId})
  * to remember the configuration.
  *
  * <p>
@@ -35,42 +41,87 @@ import java.io.IOException;
  */
 public class IJsbergLinkPlugin extends Builder {
 
-    private final String name;
+    private final String customerId;
+	private final String projectId;
+	private final String monitorUploadDirectory;
 
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+	public static final String SNAPSHOT_TIMESTAMP_FORMAT = "yyyyMMdd_HH_mm";
 
-	//TODO pass parameters here -> customer name, project name, monitor repository dir
-    @DataBoundConstructor
-    public IJsbergLinkPlugin(String name) {
-        this.name = name;
+	// Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+
+	@DataBoundConstructor
+    public IJsbergLinkPlugin(String customerId, String projectId, String monitorUploadDirectory) {
+        this.customerId = customerId;
+		this.projectId = projectId;
+		this.monitorUploadDirectory = monitorUploadDirectory;
     }
 
     /**
      * We'll use this from the <tt>config.jelly</tt>.
      */
-    public String getName() {
-        return name;
-    }
+	public String getCustomerId() {
+		return customerId;
+	}
 
-    @Override
+	public String getProjectId() {
+		return projectId;
+	}
+
+	public String getMonitorUploadDirectory() {
+		return monitorUploadDirectory;
+	}
+
+	@Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
         // This is where you 'build' the project.
-        // Since this is a dummy, we just say 'hello world' and call that a build.
-
         // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
 
+		String uploadDir = monitorUploadDirectory;
 
+        if (getDescriptor().getAlternativeUploadDirectory() != null && !"".equals(getDescriptor().getAlternativeUploadDirectory())) {
+			uploadDir = getDescriptor().getAlternativeUploadDirectory();
+		}
+		listener.getLogger().println("got request to analyze project " + projectId + " for " + customerId);
+		File uploadDirectory = new File(uploadDir);
+		if(!uploadDirectory.exists()) {
+			listener.getLogger().println("ERROR: upload directory " + uploadDirectory.getAbsolutePath() + " does not exist or is not accessible");
+			return false;
+		}
+		if(!uploadDirectory.isDirectory()) {
+			listener.getLogger().println("ERROR: " + uploadDirectory.getAbsolutePath() + " is not a directory");
+			return false;
+		}
+		FilePath path = build.getWorkspace();
+		listener.getLogger().println("zipping sources to " + uploadDirectory.getAbsolutePath());
 
-		listener.getLogger().println(build.getRootDir());
-
+		String destfileName = uploadDir + "/" + getSnapshotZipfileName(customerId, projectId, new Date()) + ".zip";
+		File file = new File(destfileName);
+		try {
+			FileOutputStream tmpFileOutput = new FileOutputStream(file);
+			path.zip(tmpFileOutput);
+			FileSupport.createFile(destfileName + ".DONE");
+		} catch (IOException e) {
+			listener.getLogger().println("exception encountered during zip process with message: " + e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+		catch (InterruptedException e) {
+			listener.getLogger().println("exception encountered during zip process with message: " + e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+		listener.getLogger().println("DONE ... created snapshot " + destfileName);
         return true;
     }
 
-    // Overridden for better type safety.
+
+	public static String getSnapshotZipfileName(String customerId, String projectId, Date snapshotTimestamp) {
+		return customerId + "." +
+				projectId + "." + new SimpleDateFormat(SNAPSHOT_TIMESTAMP_FORMAT).format(snapshotTimestamp) + ".zip";
+	}
+
+
+	// Overridden for better type safety.
     // If your plugin doesn't really define any property on Descriptor,
     // you don't have to do this.
     @Override
@@ -95,7 +146,7 @@ public class IJsbergLinkPlugin extends Builder {
          * <p>
          * If you don't want fields to be persisted, use <tt>transient</tt>.
          */
-        private boolean useFrench;
+        private String alterantiveUploadDirectory;
 
         /**
          * In order to load the persisted global configuration, you have to 
@@ -117,12 +168,17 @@ public class IJsbergLinkPlugin extends Builder {
          *      prevent the form from being saved. It just means that a message
          *      will be displayed to the user. 
          */
-        public FormValidation doCheckName(@QueryParameter String value)
+        public FormValidation doCheckAlternativeUploadDirectory(@QueryParameter String value)
                 throws IOException, ServletException {
             if (value.length() == 0)
-                return FormValidation.error("Please set a name");
-            if (value.length() < 4)
-                return FormValidation.warning("Isn't the name too short?");
+                return FormValidation.error("Please provide upload directory");
+			File file = new File(value);
+			if(!file.exists()) {
+				return FormValidation.error("Upload directory " + value + " does not exist");
+			}
+			if(!file.isDirectory()) {
+				return FormValidation.error(value + " is not a directory");
+			}
             return FormValidation.ok();
         }
 
@@ -142,7 +198,7 @@ public class IJsbergLinkPlugin extends Builder {
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             // To persist global configuration information,
             // set that to properties and call save().
-            useFrench = formData.getBoolean("useFrench");
+            alterantiveUploadDirectory = formData.getString("alternativeUploadDirectory");
             // ^Can also use req.bindJSON(this, formData);
             //  (easier when there are many fields; need set* methods for this, like setUseFrench)
             save();
@@ -150,14 +206,22 @@ public class IJsbergLinkPlugin extends Builder {
         }
 
         /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
          */
-        public boolean getUseFrench() {
-            return useFrench;
+        public String getAlternativeUploadDirectory() {
+			return alterantiveUploadDirectory;
         }
     }
+
+
+	public static void main(String[] args) throws Exception {
+		FilePath path = new FilePath(new File("C:/util/keys"));
+
+
+		String tempfileName = "C:/tmp//tmp_" + System.nanoTime() + ".zip";
+		File tmpFile = new File(tempfileName);
+
+		path.zip(new FilePath(tmpFile));
+
+	}
 }
 
