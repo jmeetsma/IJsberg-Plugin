cb6aeb607a0e556cccd88fe26898788eabc05c72
jmeetsma <jeroen@ijsberg.nl>
2015-03-13 16:12:53 +0100

    created TeamCity plugin. Made functionality buildserver independent.

--- src/main/java/org/ijsberg/ijsbergplugin/IJsbergLinkPlugin.java
+++ src/main/java/org/ijsberg/ijsbergplugin/IJsbergLinkPlugin.java
@@ -1,7 +1,6 @@
 package org.ijsberg.ijsbergplugin;
 
 import hudson.Extension;
-import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -10,24 +9,16 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import hudson.util.FormValidation;
 import net.sf.json.JSONObject;
-import org.ijsberg.iglu.logging.Level;
+import nl.ijsberg.analysis.server.buildserver.BuildServerToMonitorLink;
+import nl.ijsberg.analysis.server.buildserver.ValidationResult;
 import org.ijsberg.iglu.logging.LogEntry;
-import org.ijsberg.iglu.util.collection.ArraySupport;
-import org.ijsberg.iglu.util.io.FSFileCollection;
-import org.ijsberg.iglu.util.io.FileFilterRuleSet;
-import org.ijsberg.iglu.util.io.FileSupport;
-import org.ijsberg.iglu.util.io.ZipFileStreamProvider;
-import org.ijsberg.iglu.util.misc.StringSupport;
-import org.ijsberg.iglu.util.properties.PropertiesSupport;
+import org.ijsberg.iglu.logging.Logger;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import javax.servlet.ServletException;
 import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -45,9 +36,8 @@ import java.util.Properties;
  * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
  * method will be invoked. 
  *
- * @author Kohsuke Kawaguchi
  */
-public class IJsbergLinkPlugin extends Builder {
+public class IJsbergLinkPlugin extends Builder implements Logger {
 
     private String customerId;
 	private String projectId;
@@ -66,18 +56,9 @@ public class IJsbergLinkPlugin extends Builder {
 
 		this.monitorUploadDirectory = monitorUploadDirectory;
         this.analysisProperties = analysisProperties;
-		loadProperties(analysisProperties);
+		BuildServerToMonitorLink.throwIfPropertiesNotOk(analysisProperties, monitorUploadDirectory);
     }
 
-	private void loadProperties(String analysisProperties) throws IOException {
-		InputStream inputStream = new FileInputStream(new File(analysisProperties));
-		properties.load(inputStream);
-		this.projectId = properties.getProperty("projectName");
-		this.customerId = properties.getProperty("customerName");
-		System.out.println(new LogEntry(Level.VERBOSE, "loaded properties for customer "
-				+ this.customerId + ", project " + this.projectId));
-		inputStream.close();
-	}
 
 	/**
      * We'll use this from the <tt>config.jelly</tt>.
@@ -98,40 +79,19 @@ public class IJsbergLinkPlugin extends Builder {
 		return analysisProperties;
 	}
 
+	private PrintStream logStream = System.out;
+
 	@Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        // This is where you 'build' the project.
-        // This also shows how you can consult the global configuration of the builder
-		// reload properties on-the-fly
-		try {
-			loadProperties(analysisProperties);
-		} catch (IOException e) {
-			listener.getLogger().println(new LogEntry("ERROR: unable to reload properties " + analysisProperties, e));
-			return false;
-		}
+
+		logStream = listener.getLogger();
 
 		String uploadDir = getCurrentUploadDir(listener);
-		File uploadDirectory = new File(uploadDir);
-		if(!uploadDirectory.exists()) {
-			listener.getLogger().println("ERROR: upload directory " + uploadDirectory.getAbsolutePath() + " does not exist or is not accessible");
-			return false;
-		}
-		if(!uploadDirectory.isDirectory()) {
-			listener.getLogger().println("ERROR: " + uploadDirectory.getAbsolutePath() + " is not a directory");
-			return false;
-		}
-		String workSpacePath = build.getWorkspace().getRemote();
-		listener.getLogger().println("zipping sources from " + workSpacePath + " to " + uploadDirectory.getAbsolutePath());
 
-		String destfileName = uploadDir + "/" + getSnapshotZipfileName(customerId, projectId, new Date());
-		try {
-			zipSources(workSpacePath, destfileName, listener);
-		} catch (IOException e) {
-			listener.getLogger().println("exception encountered during zip process with message: " + e.getMessage());
-			e.printStackTrace();
-			return false;
-		}
-		listener.getLogger().println("DONE ... created snapshot " + destfileName);
+		new BuildServerToMonitorLink(analysisProperties, uploadDir, this).perform(build.getWorkspace().getRemote());
+
+		logStream = System.out;
+
         return true;
     }
 
@@ -144,64 +104,6 @@ public class IJsbergLinkPlugin extends Builder {
 		return uploadDir;
 	}
 
-	private void zipSources(String workSpacePath, String destfileName, BuildListener listener) throws IOException {
-		ZipFileStreamProvider zipFileStreamProvider = new ZipFileStreamProvider(destfileName);
-		List<String> languages = StringSupport.split(properties.getProperty("languages"));
-
-		for(String language : languages) {
-			listener.getLogger().println("zipping sources for language " + language);
-			Properties languageProperties = PropertiesSupport.getSubsection(properties, language);
-			FileFilterRuleSet fileFilterRuleSet = configureFileFilter(PropertiesSupport.getSubsection(languageProperties, "fileFilter"));
-			copyFilesToZip(workSpacePath, zipFileStreamProvider, fileFilterRuleSet);
-			Properties testFileFilterProperties = PropertiesSupport.getSubsection(languageProperties, "testFileFilter");
-			if(testFileFilterProperties != null && !testFileFilterProperties.isEmpty()){
-				FileFilterRuleSet testFileFilterRuleSet = configureFileFilter(testFileFilterProperties);
-				listener.getLogger().println("zipping TEST sources for language " + language);
-				copyFilesToZip(workSpacePath, zipFileStreamProvider, testFileFilterRuleSet);
-			}
-		}
-		zipFileStreamProvider.close();
-		FileSupport.createFile(destfileName + ".DONE");
-	}
-
-	private static void copyFilesToZip(String workSpacePath, ZipFileStreamProvider zipFileStreamProvider, FileFilterRuleSet fileFilterRuleSet) throws IOException {
-		FSFileCollection fsFileCollection = new FSFileCollection(workSpacePath, fileFilterRuleSet);
-		for(String fileName : fsFileCollection.getFileNames()) {
-			OutputStream outputStream = zipFileStreamProvider.createOutputStream(fileName);
-			File fileInCollection = fsFileCollection.getActualFileByName(fileName);
-			FileSupport.copyFileResource(fileInCollection.getAbsolutePath(), outputStream);
-			zipFileStreamProvider.closeCurrentStream();
-		}
-	}
-
-	public static FileFilterRuleSet configureFileFilter(Properties fileFilterProperties) {
-
-		FileFilterRuleSet retval = new FileFilterRuleSet().setIncludeFilesWithNameMask("*.*");
-
-		retval.setIncludeFilesWithNameMask(ArraySupport.format(getSettingArray(fileFilterProperties, "includeFilesWithName"), "|"));
-		retval.setExcludeFilesWithNameMask(ArraySupport.format(getSettingArray(fileFilterProperties, "excludeFilesWithName"), "|"));
-		retval.setIncludeFilesContainingText(getSettingArray(fileFilterProperties, "includeFilesContainingText"));
-		retval.setExcludeFilesContainingText(getSettingArray(fileFilterProperties, "excludeFilesContainingText"));
-
-		return retval.clone();
-	}
-
-
-	public static String[] getSettingArray(Properties properties, String key) {
-		String value = properties.getProperty(key);
-		if(value == null || value.isEmpty()) {
-			return new String[]{};
-		}
-		return StringSupport.split(value).toArray(new String[]{});
-	}
-
-
-
-	public static String getSnapshotZipfileName(String customerId, String projectId, Date snapshotTimestamp) {
-		return customerId + "." +
-				projectId + "." + new SimpleDateFormat(SNAPSHOT_TIMESTAMP_FORMAT).format(snapshotTimestamp) + ".zip";
-	}
-
 
 	// Overridden for better type safety.
     // If your plugin doesn't really define any property on Descriptor,
@@ -211,7 +113,21 @@ public class IJsbergLinkPlugin extends Builder {
         return (DescriptorImpl)super.getDescriptor();
     }
 
-    /**
+	public void log(LogEntry entry) {
+		logStream.println(entry);
+	}
+
+	public String getStatus() {
+		return null;
+	}
+
+	public void addAppender(Logger appender) {
+	}
+
+	public void removeAppender(Logger appender) {
+	}
+
+	/**
      * Descriptor for {@link IJsbergLinkPlugin}. Used as a singleton.
      * The class is marked as public so that it can be accessed from views.
      *
@@ -252,41 +168,24 @@ public class IJsbergLinkPlugin extends Builder {
          */
 		public FormValidation doCheckAnalysisProperties(@QueryParameter String value)
 				throws IOException, ServletException {
-			if (value.length() == 0)
-				return FormValidation.error("Please provide a path to the analysis properties file");
-			File file = new File(value);
-			if(!file.exists()) {
-				return FormValidation.error("File " + value + " does not exist or is not accessible");
-			}
-			if(file.isDirectory()) {
-				return FormValidation.error(value + " is a directory");
-			}
-			Properties properties = new Properties();
-			properties.load(new FileInputStream(new File(value)));
 
-			if(!properties.containsKey("customerName")) {
-				return FormValidation.error("Properties file " + value + " does not contain property customerName");
+			ValidationResult checkedAnalysisProperties = BuildServerToMonitorLink.checkAnalysisProperties(value);
+			if(checkedAnalysisProperties.isOk()) {
+				return FormValidation.ok();
+			} else {
+				return FormValidation.error(checkedAnalysisProperties.getLastMessage());
 			}
-			if(!properties.containsKey("projectName")) {
-				return FormValidation.error("Properties file " + value + " does not contain property projectName");
-			}
-
-			return FormValidation.ok();
 		}
 
 
 		public FormValidation doCheckMonitorUploadDirectory(@QueryParameter String value)
 				throws IOException, ServletException {
-			if (value.length() == 0)
-				return FormValidation.error("Please provide a path of the upload directory of the Monitor Server");
-			File file = new File(value);
-			if(!file.exists()) {
-				return FormValidation.error("File " + value + " does not exist or is not accessible");
+			ValidationResult checkedAnalysisProperties = BuildServerToMonitorLink.checkMonitorUploadDirectory(value);
+			if(checkedAnalysisProperties.isOk()) {
+				return FormValidation.ok();
+			} else {
+				return FormValidation.error(checkedAnalysisProperties.getLastMessage());
 			}
-			if(!file.isDirectory()) {
-				return FormValidation.error(value + " is not a directory");
-			}
-			return FormValidation.ok();
 		}
 
 
